531c0f5b2f80749cbe2fca148d48b0c3c516b7bd
acanor <acanor@gmail.com>
2015-04-28 14:14:40 -0500

    Task D completa

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
 		@products = Product.order(:title)
+		@count = increment_count_session
   end
+
+	def increment_count_session
+		if session[:counter].nil?
+			session[:counter] = 0
+		else
+			session[:counter] += 1
+		end
+	end
 end
